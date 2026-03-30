390dcfe5fabe0394f5c3a0e64c9a69d01fd71b5f
Claymaver <153468865+Claymaver@users.noreply.github.com>
2026-03-31 09:25:12 +1300

    Version Bump

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -14,6 +14,6 @@ using System.Windows;
 
 [assembly: PluginDisplayName("State Machine Editor")]
 [assembly: PluginAuthor("Claymaver")]
-[assembly: PluginVersion("1.0.0.1")]
+[assembly: PluginVersion("1.0.0.2")]
 
 [assembly: RegisterAssetDefinition("CharacterStateOwnerData", typeof(StateMachineEditorPlugin.StateMachineAssetDefinition))]
